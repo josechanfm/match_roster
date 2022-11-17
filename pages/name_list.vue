a2a308f2012426652181c58da0f52f528b18bc66
josechanfm <josechan@ipm.edu.mo>
2022-11-17 11:46:01 +0800

    set Interval & i18n

--- pages/name_list.vue
+++ pages/name_list.vue
@@ -1,46 +1,53 @@
 <template>
-  <div class="text-center bg-gray-50 text-gray-800 py-5 px-6">
+  <div class="text-center text-gray-800 py-5 px-6 ">
     <h1 class="text-5xl font-bold mt-0 mb-6">出場順序</h1>
   </div>
   <div class="flex flex-wrap">
     <div class="w-full">
-      <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+      <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row px-4">
         <li class="-mb-px mr-2 last:mr-0 flex-auto text-center" v-for="(mat, index) of players">
-          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal"
+          <a class="text-3xl font-bold uppercase px-5 py-3 shadow-lg rounded-lg block leading-normal"
             v-on:click="toggleTabs(index)"
-            v-bind:class="{ 'text-pink-600 bg-white': openTab !== index, 'text-white bg-pink-600': openTab === index }">
-            Mat {{ mat.mat }}
+            v-bind:class="{ 'text-pink-600 bg-amber-100': openTab !== index, 'text-white bg-pink-600': openTab === index }">
+            {{$t('mat') }} {{ mat.mat }}
           </a>
         </li>
       </ul>
 
-      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+      <div class="relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded">
         <div class="px-4 py-5 flex-auto">
           <div class="tab-content tab-space">
             <span v-for="(mat, index) of players">
               <div v-bind:class="{ 'hidden': openTab !== index, 'block': openTab === index }">
-
                 <div class="flex flex-col">
                   <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                      <div class="overflow-hidden">
-                        <table class="min-w-full">
-                          <thead class="bg-white border-b">
+                      <div class="overflow-hidden rounded-lg">
+                        <table class="min-w-full border-separate border">
+                          <thead class="border-b">
                             <tr>
-                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                White
+                              <th scope="col" class="text-xl font-medium bg-amber-100 text-gray-900 px-1 py-1 text-center">
+                                #
                               </th>
-                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                Blue
+                              <th scope="col" class="text-3xl font-medium bg-white text-gray-900 px-6 py-4 text-center">
+                                {{$t('white') }}
+                              </th>
+                              <th scope="col" class="text-3xl font-medium bg-blue-300 text-gray-900 px-6 py-4 text-center">
+                                {{$t('blue') }}
                               </th>
                             </tr>
                           </thead>
                           <tbody>
-                            <tr class="bg-white-100 border-b" v-for="player in mat.players">
-                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <tr class="bg-white-100 border-b " v-for="player in mat.players">
+                              <td class="text-xl bg-amber-100 text-gray-900 font-light px-0 py-1 whitespace-nowrap text-center">
+                                <div class="text-3xl">{{ player.category }}</div>
+                                <div class="text-2xl">{{ player.gender }} {{ player.weight }}</div>
+                                
+                              </td>
+                              <td class="text-5xl bg-white text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                 {{ player.white_abbr }} - {{ player.white_display_name }}
                               </td>
-                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              <td class="text-5xl bg-blue-300 text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                 {{ player.blue_abbr }} - {{ player.blue_display_name }}
                               </td>
                             </tr>
@@ -60,23 +67,42 @@
 
     </div>
   </div>
-  <h1>Demo of remote data</h1>
-  <hr>
-  <ul>
-    <li v-for="mountain of mountains">{{mountain.title}}</li>
-  </ul>
+
+  <div class="absolute bottom-0 right-0 h-8 w-20">
+    <form>
+          <select id="locale-select" v-model="$i18n.locale">
+            <option value="en">en</option>
+            <option value="zh">zh</option>
+          </select>
+        </form>
+  </div>  
 </template>
   
 <script setup>
-import players from "/contents/players.json"
+//import players from "/contents/players.json"
+import { onMounted} from "vue";
 
 const openTab = ref(0);
-const { data: mountains } = await useFetch('https://api.nuxtjs.dev/mountains');
+const matNum=ref(0);
+const timer=ref(0);
+const { data: players } = await useFetch('http://localhost:3000/players.json');
+
 const toggleTabs = (tabNumber) => {
   openTab.value = tabNumber;
-}
+};
+
+const changeTabs = ()=>{
+  matNum.value=players._value.length;
+  openTab.value=(openTab.value+1)%matNum.value;
+};
+
+const intervalHandel = setInterval(changeTabs,3000);
 
 </script>
   
-  
+<style>
+body{
+  background-color: lightgray;
+}
+</style>
   
